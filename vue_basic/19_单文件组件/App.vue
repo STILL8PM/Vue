8f7af4126bd32efa0f99f4c175cc764f26519e81
pw <1040479402@qq.com>
2022-03-05 21:10:43 +0800

    20220305

--- vue_basic/19_单文件组件/App.vue
+++ vue_basic/19_单文件组件/App.vue
@@ -1,20 +1,21 @@
 <template>
-	<div>
-		<School></School>
-		<Student></Student>
-	</div>
+  <div>
+    <School></School>
+    <Student></Student>
+  </div>
 </template>
 
 <script>
-	//引入组件
-	import School from './School.vue'
-	import Student from './Student.vue'
-
-	export default {
-		name:'App',
-		components:{
-			School,
-			Student
-		}
-	}
+import School from "./School.vue";
+import Student from "./Student.vue";
+export default {
+  name: "App",
+  components: {
+    School,
+    Student,
+  },
+};
 </script>
+
+<style>
+</style>
