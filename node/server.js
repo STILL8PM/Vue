e1636a6bfa3f147cfac920bcbcf6a9c61d358a30
pw <1040479402@qq.com>
2022-03-09 20:32:44 +0800

    20220309

---
+++ node/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express, static(__dirname + '/static'))
+
+app.get('/person', (request, response) => {
+    response.send({
+        name: 'Tom',
+        age: 18
+    })
+})
+
+app.listen(5005, (err) => {
+    if (!err) console.log('服务器启动成功了！');
+})
