b08f7a7695fd05dfeab1a6360444adb686bc12d6
pw <1040479402@qq.com>
2022-03-09 18:57:42 +0800

    20220309

--- vue_test/src/pages/About.vue
+++ vue_test/src/pages/About.vue
@@ -5,5 +5,26 @@
 <script>
 export default {
   name: "About",
+
+  //组件内守卫
+  //通过路由规则，进入该组件时被调用
+  beforeRouteEnter(to, from, next) {
+    // console.log("beforeRouteEnter");
+    if (to.meta.isAuth) {//判断是否需要鉴权
+        if (localStorage.getItem('school') === 'atguigu') {
+            next()
+        } else {
+            alert('无权查看')
+        }
+    } else {
+        next()
+    }
+    next()
+  },
+  //通过路由规则，离开该组件时被调用
+  beforeRouteLeave(to, from, next) {
+    console.log("beforeRouteLeave");
+    next()
+  },
 };
 </script>
--- vue_test/src/router/index.js
+++ vue_test/src/router/index.js
@@ -8,29 +8,54 @@ import Message from '../pages/Message.vue'
 import Detail from '../pages/Detail.vue'
 
 //创建并暴露一个路由器
-export default new VueRouter({
+const router = new VueRouter({
     routes: [
         {
             name: 'guanyu',
             path: '/about',
-            component: About
+            component: About,
+            meta: { title: '关于',isAuth: true }
         },
         {
+            name: 'zhuye',
             path: '/home',
             component: Home,
+            meta: { title: '主页' },
             children: [
                 {
+                    name: 'xinwen',
                     path: 'news',
                     component: News,
+                    meta: { title: '新闻', isAuth: true },
+                    /* 独享路由守卫 */
+                    /* beforeEnter: (to, from, next) => {
+                        console.log('独享路由守卫', to, from);
+                        if (to.meta.isAuth) {//判断是否需要鉴权
+                            if (localStorage.getItem('school') === 'atguigu') {
+                                // document.title = to.meta.title || '硅谷系统'
+                                next()
+                            } else {
+                                alert('无权查看')
+                            }
+                        } else {
+                            // document.title = to.meta.title || '硅谷系统'
+                            next()
+                        }
+                    } */
+
                 },
                 {
+                    name: 'xiaoxi',
                     path: 'message',
                     component: Message,
+                    meta: { title: '消息', isAuth: true },
                     children: [
                         {
                             name: 'xiangqing',
                             path: 'detail/:id/:title',//占位
                             component: Detail,
+                            meta: { title: '详情', isAuth: true },
+
 
                             //props的第一种写法，值为对象，该对象中的所有key-value都会以props的形式传给Detail组件
                             // props:{a:1,b:'hello'}
@@ -53,3 +78,32 @@ export default new VueRouter({
         },
     ]
 })
+
+//全局前置路由守卫————初始化调用，每次路由切换之前被调用
+/* router.beforeEach((to, from, next) => {
+    console.log('前置路由守卫', to, from);
+    //if (to.name === 'xinwen' || to.name === 'xiaoxi'){}      
+    // if (to.path === '/home/news' || to.path === '/home/message'){}
+    if (to.meta.isAuth) {//判断是否需要鉴权
+        if (localStorage.getItem('school') === 'atguigu') {
+            // document.title = to.meta.title || '硅谷系统'
+            next()
+        } else {
+            alert('无权查看')
+        }
+    } else {
+        // document.title = to.meta.title || '硅谷系统'
+        next()
+    }
+}) */
+
+//全局后置路由守卫————初始化调用，每次路由切换之后被调用
+/* router.afterEach((to, from) => {
+    console.log('后置路由守卫', to, from);
+    if (to.meta.title) {
+        document.title = to.meta.title//最佳
+    } else {
+        document.title = "硅谷系统"
+    }
+}) */
+export default router
